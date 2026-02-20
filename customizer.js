4c5bb5ab5fdc366ea23159ce1b728ca199916077
kiaraseong01-art <kiaraseong01@gmail.com>
2026-02-19 19:38:36 -0800

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -869,7 +869,7 @@ function initCustomizer(root) {
       // ✅ NORMAL
       if (size === "small") {
         if (area === "bottom") return step(19, 18, 16, 16);
-        return step(16, 14, 12, 10); // top
+        return step(16, 14, 12, 11); // top
       }
 
       if (size === "medium") return step(22, 20, 18, 16);
