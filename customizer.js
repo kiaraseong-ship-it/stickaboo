b8f548e1aadba9c01dceb14840b55f5bf38a9aca
kiaraseong01-art <kiaraseong01@gmail.com>
2026-07-14 01:21:38 -0700

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -1358,11 +1358,11 @@ function initCustomizer(root) {
       // ✅ NORMAL
       if (size === "small") {
         const base = step(16, 14, 13, 11.5);
-        return area === "bottom" ? base * 0.7 : base;
+        return area === "bottom" ? base * 0.9 : base;
       }
       if (size === "medium") {
         const base = step(22, 20, 18, 17);
-        return area === "bottom" ? base * 0.7 : base;
+        return area === "bottom" ? base * 0.9 : base;
       }
       if (size === "large") {
         if (area === "top") return twoLines ? step(20, 20, 20, 20) : step(32, 26, 24, 20);
