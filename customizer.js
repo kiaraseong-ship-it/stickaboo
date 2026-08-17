381c3f7ec18f5762f2df07e4d285d1cc9a2c7aba
kiaraseong01-art <kiaraseong01@gmail.com>
2026-08-16 22:45:07 -0700

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -1288,7 +1288,7 @@ function initCustomizer(root) {
 
       // ✅ NAMEONLY
       if (theme === "nameonly") {
-        if (size === "small") return step(24, 22, 20, 16);
+        if (size === "small") return step(20, 18, 16, 14);
         if (area === "medium") return twoLines ? step(20, 18, 16, 14) : step(40, 30, 26, 22);
 
         if (size === "large") {
