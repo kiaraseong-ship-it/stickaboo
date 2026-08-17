89669238984e5fe5fbafeb026803205e8c5bf78d
kiaraseong01-art <kiaraseong01@gmail.com>
2026-08-16 22:47:23 -0700

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -1178,7 +1178,7 @@ function initCustomizer(root) {
       }
 
       if (size === "medium") {
-        return Math.max(10, Math.round(fs * 0.9)) + "px";
+        return Math.max(10, Math.round(fs * 1)) + "px";
       }
 
 
